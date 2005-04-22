6cfa7f496f9de7d6d629ef1e3c5e2bd956f41b73
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2005-04-22 16:40:31 +0000

    elaborated javadoc for the FI team.

--- runtime/src/com/sun/xml/bind/v2/runtime/unmarshaller/XmlVisitor.java
+++ runtime/src/com/sun/xml/bind/v2/runtime/unmarshaller/XmlVisitor.java
@@ -1,7 +1,5 @@
 package com.sun.xml.bind.v2.runtime.unmarshaller;
 
-import javax.xml.namespace.NamespaceContext;
-
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 
@@ -10,18 +8,28 @@ import org.xml.sax.SAXException;
  *
  * Implemented by the unmarshaller and called by the API-specific connectors.
  *
- * <h2>Event Contracts</h2>
- * <ul>
- *  <li>Text events may not be called in a row.
- * </ul>
+ * <h2>Event Call Sequence</h2>
+ *
+ * The {@link XmlVisitor} expects the event callbacks in the following order:
+ * <pre>
+ * CALL SEQUENCE := startDocument ELEMENT endDocument
+ * ELEMENT       := startPrefixMapping ELEMENT endPrefixMapping
+ *               |  startElement BODY endElement
+ * BODY          := text? (ELEMENT text?)*
+ * </pre>
+ * Note in particular that text events may not be called in a row;
+ * consecutive characters (even those separated by PIs and comments)
+ * must be reported as one event, unlike SAX.
+ *
+ * <p>
+ * All namespace URIs, local names, and prefixes of element and attribute
+ * names must be interned. qnames need not be interned.
  *
  *
- * <h2>Connector's Responsibilities</h2>
- * <ul>
- *  <li>Maintains {@link NamespaceContext}.
- * </ul>
- *
- *
+ * <h2>Typed PCDATA</h2>
+ * For efficiency, JAXB RI defines a few {@link CharSequence} implementations
+ * that can be used as a parameter to the {@link #text(CharSequence)} method.
+ * For example, see {@link Base64Data}.
  *
  * <h2>Error Handling</h2>
  * The visitor may throw {@link SAXException} to abort the unmarshalling process
@@ -37,6 +45,11 @@ public interface XmlVisitor {
      * Notifies a start tag of a new element.
      *
      * namespace URIs and local names must be interned.
+     *
+     * @param atts
+     *      the attributes of this element. must not be null.
+     *      need not be immutable; the caller may reuse the same
+     *      {@link Attributes} object for multiple elements.
      */
     void startElement( String nsUri, String localName, String qname, Attributes atts ) throws SAXException;
     void endElement( String nsUri, String localName, String qname ) throws SAXException;
@@ -54,7 +67,9 @@ public interface XmlVisitor {
      * Text events.
      *
      * @param pcdata
-     *      represents characters.
+     *      represents character data. This object can be mutable
+     *      (such as {@link StringBuilder}); it only needs to be fixed
+     *      while this method is executing.
      */
     void text( CharSequence pcdata ) throws SAXException;
 
