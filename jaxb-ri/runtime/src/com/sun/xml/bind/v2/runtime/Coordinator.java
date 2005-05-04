1ea384f9c4af177c94e838e17e8f07d68341cd35
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2005-05-04 03:57:18 +0000

    implemented the marshal-time validation.

--- jaxb-ri/runtime/src/com/sun/xml/bind/v2/runtime/Coordinator.java
+++ jaxb-ri/runtime/src/com/sun/xml/bind/v2/runtime/Coordinator.java
@@ -3,9 +3,17 @@ package com.sun.xml.bind.v2.runtime;
 import java.util.HashMap;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
+import javax.xml.bind.ValidationEventHandler;
+import javax.xml.bind.ValidationEvent;
+import javax.xml.bind.ValidationEventLocator;
+import javax.xml.bind.helpers.ValidationEventImpl;
 
 import com.sun.xml.bind.v2.ClassFactory;
 import com.sun.xml.bind.v2.runtime.unmarshaller.UnmarshallingContext;
+
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.SAXException;
 
 /**
  * Object that coordinates the marshalling/unmarshalling.
@@ -22,10 +30,14 @@ import com.sun.xml.bind.v2.runtime.unmarshaller.UnmarshallingContext;
  * for an entirely different episode.)
  *
  * This class also maintains the user-configured instances of {@link XmlAdapter}s.
- * 
+ *
+ * <p>
+ * This class implements {@link ErrorHandler} and propages erros to this object
+ * as the {@link ValidationEventHandler}, which will be implemented in a derived class.
+ *
  * @author Kohsuke Kawaguchi
  */
-public class Coordinator {
+public abstract class Coordinator implements ErrorHandler, ValidationEventHandler {
     
     private final HashMap<Class<? extends XmlAdapter>,XmlAdapter> adapters =
             new HashMap<Class<? extends XmlAdapter>,XmlAdapter>();
@@ -111,4 +123,52 @@ public class Coordinator {
             return new Coordinator[1];
         }
     };
+
+
+//
+//
+// ErrorHandler implementation
+//
+//
+    /**
+     * Gets the current location. Used for reporting the error source location.
+     */
+    protected abstract ValidationEventLocator getLocation();
+
+    public final void error(SAXParseException exception) throws SAXException {
+        propagateEvent( ValidationEvent.ERROR, exception );
+    }
+
+    public final void warning(SAXParseException exception) throws SAXException {
+        propagateEvent( ValidationEvent.WARNING, exception );
+    }
+
+    public final void fatalError(SAXParseException exception) throws SAXException {
+        propagateEvent( ValidationEvent.FATAL_ERROR, exception );
+    }
+
+    private void propagateEvent( int severity, SAXParseException saxException )
+        throws SAXException {
+
+        ValidationEventImpl ve =
+            new ValidationEventImpl( severity, saxException.getMessage(), getLocation() );
+
+        Exception e = saxException.getException();
+        if( e != null ) {
+            ve.setLinkedException( e );
+        } else {
+            ve.setLinkedException( saxException );
+        }
+
+        // call the client's event handler.  If it returns false, then bail-out
+        // and terminate the unmarshal operation.
+        boolean result = handleEvent( ve );
+        if( ! result ) {
+            // bail-out of the parse with a SAX exception, but convert it into
+            // an UnmarshalException back in in the AbstractUnmarshaller
+            throw saxException;
+        }
+    }
+
+
 }
