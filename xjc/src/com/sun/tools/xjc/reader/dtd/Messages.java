aed549dc22d448bd35fc5ff6e32cf54e5aaf1f09
Bhakti Mehta <bhakti.mehta@oracle.com>
2005-05-06 00:02:18 +0000

    Renaming the Messages.properties file to MessageBundle because as part of jdk build process the properties files were compiled into java classes and that conflicted with Messages.java

--- xjc/src/com/sun/tools/xjc/reader/dtd/Messages.java
+++ xjc/src/com/sun/tools/xjc/reader/dtd/Messages.java
@@ -15,7 +15,7 @@ class Messages
 {
     /** Loads a string resource and formats it with specified arguments. */
     static String format( String property, Object... args ) {
-        String text = ResourceBundle.getBundle(Messages.class.getName()).getString(property);
+        String text = ResourceBundle.getBundle(Messages.class.getPackage().getName() + ".MessageBundle").getString(property);
         return MessageFormat.format(text,args);
     }
     
