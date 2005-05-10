5ef16ec88cdb989090156c058b1a018edc9da4e9
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2005-05-10 20:58:03 +0000

    added new integration targets

--- tools/scripts/integrate-txw2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# shell script to integrate txw2 from Hudson
-cd $JAXB_HOME
-wget -O tools/lib/rebundle/compiler/txw2.jar \
-	http://kohsuke.sfbay/hudson/job/txw2/lastSuccessfulBuild/artifact/txw2/build/txw2.jar
-wget -O tools/lib/util/txwc2.jar \
-	http://kohsuke.sfbay/hudson/job/txw2/lastSuccessfulBuild/artifact/txw2/build/txwc2.jar
-wget -O tools/lib/src/txw2-src.zip \
-	http://kohsuke.sfbay/hudson/job/txw2/lastSuccessfulBuild/artifact/txw2/build/txw2-src.zip
