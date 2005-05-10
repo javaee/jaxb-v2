30ec37ff8fdfdae0bcb4765aa97b21cccf68ff6d
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2005-05-10 22:29:05 +0000

    added a typed CharSequence for int[] as a first step of FI integration.

---
+++ jaxb-ri/runtime/src/com/sun/xml/bind/v2/runtime/unmarshaller/IntArrayData.java
@@ -0,0 +1,79 @@
+package com.sun.xml.bind.v2.runtime.unmarshaller;
+
+/**
+ * Typed {@link CharSequence} for int[].
+ *
+ * <p>
+ * Fed to unmarshaller when the 'text' data is actually
+ * a virtual image of int array.
+ *
+ * <p>
+ * This class holds int[] as a triplet of (data,start,len)
+ * where 'start' and 'len' represents the start position of the
+ * data and the length.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class IntArrayData implements CharSequence {
+
+    private int[] data;
+    private int start;
+    private int len;
+
+    /**
+     * String representation of the data. Lazily computed.
+     */
+    private StringBuilder literal;
+
+
+    public IntArrayData(int[] data, int start, int len) {
+        set(data, start, len);
+    }
+
+    public IntArrayData() {
+    }
+
+    /**
+     * Sets the int[] data to this object.
+     *
+     * <p>
+     * This method doesn't make a copy for a performance reason.
+     * The caller is still free to modify the array it passed to this method,
+     * but he should do so with a care. The unmarshalling code isn't expecting
+     * the value to be changed while it's being routed.
+     */
+    public void set(int[] data, int start, int len) {
+        this.data = data;
+        this.start = start;
+        this.len = len;
+        this.literal = null;
+    }
+
+    public int length() {
+        return getLiteral().length();
+    }
+
+    public char charAt(int index) {
+        return getLiteral().charAt(index);
+    }
+
+    public CharSequence subSequence(int start, int end) {
+        return getLiteral().subSequence(start,end);
+    }
+
+    /**
+     * Computes the literal form from the data.
+     */
+    private StringBuilder getLiteral() {
+        if(literal!=null)   return literal;
+
+        literal = new StringBuilder();
+        int p = start;
+        for( int i=len; i>0; i-- ) {
+            if(literal.length()>0)  literal.append(' ');
+            literal.append(data[p++]);
+        }
+
+        return literal;
+    }
+}
