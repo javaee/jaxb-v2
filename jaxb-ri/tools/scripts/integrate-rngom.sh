38b6876b4b83084c25eddad5028219eee8236bb6
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2005-05-10 20:58:03 +0000

    added new integration targets

--- jaxb-ri/tools/scripts/integrate-rngom.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# shell script to integrate xsom from Hudson
-cd $JAXB_HOME
-wget -O tools/lib/rebundle/compiler/rngom.jar \
-	http://kohsuke.sfbay/hudson/job/rngom/lastSuccessfulBuild/artifact/rngom/rngom/build/rngom.jar
-wget -O tools/lib/src/rngom-src.zip \
-	http://kohsuke.sfbay/hudson/job/rngom/lastSuccessfulBuild/artifact/rngom/rngom/build/rngom-src.zip
