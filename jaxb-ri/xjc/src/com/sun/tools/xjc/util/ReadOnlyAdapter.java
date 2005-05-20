ff770127410b0152798a650523ce29aef9381f4f
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2005-05-20 16:51:40 +0000

    // the unmarshaller uses this method         // to get the current collection object from the property.         // so we can't just throw UnsupportedOperationException here

--- jaxb-ri/xjc/src/com/sun/tools/xjc/util/ReadOnlyAdapter.java
+++ jaxb-ri/xjc/src/com/sun/tools/xjc/util/ReadOnlyAdapter.java
@@ -9,6 +9,9 @@ import javax.xml.bind.annotation.adapters.XmlAdapter;
  */
 public abstract class ReadOnlyAdapter<OnTheWire,InMemory> extends XmlAdapter<OnTheWire,InMemory> {
     public final OnTheWire marshal(InMemory onTheWire) {
-        throw new UnsupportedOperationException();
+        // the unmarshaller uses this method
+        // to get the current collection object from the property.
+        // so we can't just throw UnsupportedOperationException here
+        return null;
     }
 }
