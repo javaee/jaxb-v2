40362c2694c668fa1931753dfc940ae756131906
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2005-06-15 21:18:09 +0000

    fixed a bug where the BIXPluginCustomization doesn't carry the location information.

--- jaxb-ri/xjc/test/com/sun/tools/xjc/addon/DebugPlugin.java
+++ jaxb-ri/xjc/test/com/sun/tools/xjc/addon/DebugPlugin.java
@@ -1,12 +1,10 @@
 package com.sun.tools.xjc.addon;
 
-import java.io.IOException;
-import java.util.List;
 import java.util.Arrays;
+import java.util.List;
 
-import com.sun.tools.xjc.Plugin;
 import com.sun.tools.xjc.Options;
-import com.sun.tools.xjc.BadCommandLineException;
+import com.sun.tools.xjc.Plugin;
 import com.sun.tools.xjc.outline.Outline;
 
 import org.xml.sax.ErrorHandler;
@@ -29,5 +27,9 @@ public class DebugPlugin extends Plugin {
 
     public List<String> getCustomizationURIs() {
         return Arrays.asList("http://jaxb.dev.java.net/test");
+    }
+
+    public boolean isCustomizationTagName(String nsUri, String localName) {
+        return true;
     }
 }
