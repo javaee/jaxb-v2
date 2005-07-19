4026311a9a7a494ecf8e6ea88cd77e57770a882e
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2005-07-19 00:05:06 +0000

    there were too many files and it actually breaks the java.net system. trying another way to add them

--- jaxb-ri/deploy-architecture-document.sh
+++ jaxb-ri/deploy-architecture-document.sh
@@ -1,12 +1,21 @@
-#!/bin/sh -x
+#!/bin/bash -x
 #
 # generate the architecture document and deploy that into the java.net CVS repository
 # 
+
 ant architecture-document
-cd build/javadoc
-cvs "-d:pserver:kohsuke@kohsuke.sfbay:/cvs" -z3 import -ko -W "*.png -k 'b'" -W "*.gif -k 'b'" -m "deploying the new web contents" jaxb-architecture-document/www/doc site-deployment t`date +%Y%m%d-%H%M%S`
-cd ..
-cvs "-d:pserver:kohsuke@kohsuke.sfbay:/cvs" -z3 co -l -d jaxb-architecture-document-www jaxb-architecture-document/www
+
+cd build
+
+cvs "-d:pserver:kohsuke@kohsuke.sfbay:/cvs" -z9 co -d jaxb-architecture-document-www jaxb-architecture-document/www
+
 cd jaxb-architecture-document-www
+
+cp -R ../javadoc/* doc
+
+# swallow the error code
+find . -name CVS -prune -or -exec cvs add {} \+ -or -true
+
 date >> update.html
-cvs commit -m "to work around a bug in java.net web updater" update.html
+
+cvs commit -m "to work around a bug in java.net web updater"
