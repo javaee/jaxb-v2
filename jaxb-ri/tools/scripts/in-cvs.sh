6ae9f98ee05c3d3e9b9478e264d95af9f03eade3
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2005-07-19 03:37:27 +0000

    still trying to get this thing work

--- jaxb-ri/tools/scripts/in-cvs.sh
+++ jaxb-ri/tools/scripts/in-cvs.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
+#!/bin/sh -x
 # checks if the argument is under the control of CVS.
 # if it's in, return 0, otherwise 1
 ent=`dirname $1`/CVS/Entries
-if [ ! -e $ent ];
+if [ ! -e $ent ]
 then
   # no entry file
   exit 1
 fi
 
 grep "\/`basename $1`\/" $ent > /dev/null
-if [ $? -eq 0 ];
+if [ $? -eq 0 ]
 then
   # found
   exit 0
