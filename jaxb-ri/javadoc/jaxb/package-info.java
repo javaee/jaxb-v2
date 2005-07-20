39ee09f093589f7dd4c9b28c89c3ceaf1c1e9cb5
Joseph Fialli <joe.fialli@oracle.com>
2005-07-20 23:10:21 +0000

    Added modules/libraries relationship diagram for architecture document.

--- jaxb-ri/javadoc/jaxb/package-info.java
+++ jaxb-ri/javadoc/jaxb/package-info.java
@@ -1,12 +1,41 @@
 /**
  * <h1>JAXB RI Architecture Document</h1>.
  *
- * <p>
- * JAXB RI uses the following major libraries extensively.
- * The parenthesis shows which part of the JAXB RI depends on it.
+ * <h2>JAXB RI Major Modules and Libraries</h2>
+ * <div>
+ *   <img src="doc-files/jaxbModulesLibs.png"/>
+ * </div>
  *
+ *
+ * <h2>Modules</h2>
+ * <p>
+ * The JAXB RI consists of the following major modules.
+ *
+ * <dl>
+ *  <dt>{@link com.sun.xml.bind.v2 runtime}
+ *  <dd>
+ *    runtime module is available at application runtime and provide the actual
+ *    XML unmarshalling/marshalling capability. Notably, it implements {@link JAXBContext}.
+ *
+ *  <dt>{@link com.sun.tools.xjc XJC}
+ *  <dd>
+ *    The schema compiler.
+ *
+ *  <dt>{@link com.sun.xml.bind.v2.schemagen.XmlSchemaGenerator schemagen}
+ *  <dd>
+ *    The XML Schema generator. For historical reason it lives in its own module.
+ *
+ *  <dt>{@link com.sun.xml.bind.api runtime-API}</a>
+ *  <dd>
+ *    A part of the runtime that defines the contract between the JAXB RI and
+ *    the JAX-WS RI.
+ *
+ * </dl>
  *
  * <h2>Libraries</h2>
+ * <p>
+ * JAXB RI uses the following major libraries extensively.
+ * The parenthesis shows which modules of the JAXB RI depends on it.
  * <dl>
  *  <dt><a href="../com/sun/codemodel/package-summary.html#package_description">CodeModel</a> (XJC)
  *  <dd>
@@ -20,36 +49,11 @@
  *  <dd>
  *    Library for parsing RELAX NG into in-memory representation
  *
- *  <dt><a href="#">TXW</a> (runtime)
+ *  <dt><a href="#">TXW</a> (runtime,schemagen)
  *  <dd>
  *    Library for writing XML
  * </dl>
  *
- *
- * <h2>Modules</h2>
- * <p>
- * The rest of the JAXB RI "proper" consists of the following major modules.
- *
- * <dl>
- *  <dt>{@link com.sun.xml.bind.v2 runtime}
- *  <dd>
- *    runtime module is available at application runtime and provide the actual
- *    XML unmarshalling/marshalling capability. Notably, it implements {@link JAXBContext}.
- *
- *  <dt>{@link com.sun.xml.bind.api runtime-API}</a>
- *  <dd>
- *    A part of the runtime that defines the contract between the JAXB RI and
- *    the JAX-WS RI.
- *
- *  <dt>{@link com.sun.xml.bind.v2.schemagen.XmlSchemaGenerator schemagen}
- *  <dd>
- *    The XML Schema generator. For historical reason it lives in its own module.
- *
- *
- *  <dt>{@link com.sun.tools.xjc XJC}
- *  <dd>
- *    The schema compiler.
- * </dl>
  *
  * @ArchitectureDocument
  */
