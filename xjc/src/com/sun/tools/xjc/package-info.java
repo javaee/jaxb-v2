2cc83045103bd461137796c0ee8dae7ad3babeb9
Joseph Fialli <joe.fialli@oracle.com>
2005-07-20 23:14:03 +0000

    Added xjc architecture diagram for architecture document.

--- xjc/src/com/sun/tools/xjc/package-info.java
+++ xjc/src/com/sun/tools/xjc/package-info.java
@@ -5,20 +5,25 @@
  * This module contains the code that implements the schema compiler 'XJC'.
  *
  *
+ * <h2>XJC Architecture Diagram</h2>
+ * <div>
+ *   <img src="doc-files/xjcArchitecture.png"/>
+ * </div>
+ *
  * <h2>Overview</h2>
  * <p>
  * XJC consists of the following major components.
  * <dl>
+ *  <dt>{@link com.sun.tools.xjc.reader Schema reader}
+ *  <dd>
+ *   Schema readers read XML Schema documents (or DTD, RELAX NG, ...)
+ *   and builds a model.
+ *
  *  <dt>{@link com.sun.tools.xjc.model Model}
  *  <dd>
  *   Model represents the 'blueprint' of the code to be generated.
  *   Model talks in terms of higher level constructs like 'class' and 'property'
  *   without getting too much into the details of the Java source code.
- *
- *  <dt>{@link com.sun.tools.xjc.reader Schema reader}
- *  <dd>
- *   Schema readers read XML Schema documents (or DTD, RELAX NG, ...)
- *   and builds a model.
  *
  *  <dt>{@link com.sun.tools.xjc.generator Code generator}
  *  <dd>
