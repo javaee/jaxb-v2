0661df1af0984db45a28929e4d92ac397c25d34d
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2005-08-19 18:50:31 +0000

    moved the contents from package.html to package-info.java

--- jaxb-ri/runtime/src/com/sun/xml/bind/v2/model/runtime/package-info.java
+++ jaxb-ri/runtime/src/com/sun/xml/bind/v2/model/runtime/package-info.java
@@ -1,4 +1,11 @@
-
+/**
+ * The specialization of {@link com.sun.xml.bind.v2.model.core} for the runtime.
+ *
+ * <p>
+ * This package offers stronger types and additional methods available at the runtime
+ * to access objects according to the model. Most of the interfaces correspond one to one
+ * with the core package, and they are simply named with {@code Runtime} prefix.    
+ */
 @XmlSchema(namespace="http://jaxb.dev.java.net/xjc/model",elementFormDefault=XmlNsForm.QUALIFIED)
 package com.sun.xml.bind.v2.model.runtime;
 
