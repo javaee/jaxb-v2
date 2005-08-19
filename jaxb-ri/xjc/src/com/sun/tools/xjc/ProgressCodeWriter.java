4927e3872dc6aa1c9d26ba63f3ac04be19cbac1a
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2005-08-19 21:34:24 +0000

    fixed a bug where the progress report wasn't made for Java source files.

--- jaxb-ri/xjc/src/com/sun/tools/xjc/ProgressCodeWriter.java
+++ jaxb-ri/xjc/src/com/sun/tools/xjc/ProgressCodeWriter.java
@@ -3,6 +3,7 @@ package com.sun.tools.xjc;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.Writer;
 
 import com.sun.codemodel.CodeWriter;
 import com.sun.codemodel.JPackage;
@@ -21,13 +22,21 @@ final class ProgressCodeWriter extends FilterCodeWriter {
 
     private final XJCListener progress;
 
+    public Writer openSource(JPackage pkg, String fileName) throws IOException {
+        report(pkg,fileName);
+        return super.openSource(pkg, fileName);
+    }
+
     public OutputStream openBinary(JPackage pkg, String fileName) throws IOException {
-        String name = pkg.name().replace('.',File.separatorChar);
+        report(pkg,fileName);
+        return super.openBinary(pkg,fileName);
+    }
+
+    private void report(JPackage pkg, String fileName) {
+        String name = pkg.name().replace('.', File.separatorChar);
         if(name.length()!=0)    name +=     File.separatorChar;
         name += fileName;
 
         progress.generatedFile(name);
-
-        return super.openBinary(pkg,fileName);
     }
 }
