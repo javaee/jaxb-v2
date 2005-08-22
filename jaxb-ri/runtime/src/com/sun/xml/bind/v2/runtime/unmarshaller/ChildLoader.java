982aa1140c27b5606fdfa3e59f350ba424794b1d
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2005-08-22 18:27:57 +0000

    added an assertion.

--- jaxb-ri/runtime/src/com/sun/xml/bind/v2/runtime/unmarshaller/ChildLoader.java
+++ jaxb-ri/runtime/src/com/sun/xml/bind/v2/runtime/unmarshaller/ChildLoader.java
@@ -1,6 +1,10 @@
 package com.sun.xml.bind.v2.runtime.unmarshaller;
 
 /**
+ * Pair of {@link Loader} and {@link Receiver}.
+ *
+ * Used by {@link StructureLoader}.
+ * 
  * @author Kohsuke Kawaguchi
  */
 public final class ChildLoader {
@@ -8,6 +12,7 @@ public final class ChildLoader {
     public final Receiver receiver;
 
     public ChildLoader(Loader loader, Receiver receiver) {
+        assert loader!=null;
         this.loader = loader;
         this.receiver = receiver;
     }
