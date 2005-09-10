40c1fed4e39bd898e70a60c207f30286ad1991de
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2005-09-10 19:10:46 +0000

    replaced copyright by the new CDDL version

--- txw2/src/runtime/com/sun/xml/txw2/NamespaceSupport.java
+++ txw2/src/runtime/com/sun/xml/txw2/NamespaceSupport.java
@@ -1,8 +1,28 @@
+/*
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the "License").  You may not use this file except
+ * in compliance with the License.
+ * 
+ * You can obtain a copy of the license at
+ * https://jwsdp.dev.java.net/CDDLv1.0.html
+ * See the License for the specific language governing
+ * permissions and limitations under the License.
+ * 
+ * When distributing Covered Code, include this CDDL
+ * HEADER in each file and include the License file at
+ * https://jwsdp.dev.java.net/CDDLv1.0.html  If applicable,
+ * add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your
+ * own identifying information: Portions Copyright [yyyy]
+ * [name of copyright owner]
+ */
+
 // NamespaceSupport.java - generic Namespace support for SAX.
 // http://www.saxproject.org
 // Written by David Megginson
 // This class is in the Public Domain.  NO WARRANTY!
-// $Id: NamespaceSupport.java,v 1.1 2005-04-14 21:45:18 kohsuke Exp $
+// $Id: NamespaceSupport.java,v 1.2 2005-09-10 19:10:42 kohsuke Exp $
 
 package com.sun.xml.txw2;
 
--- xsom/src/com/sun/xml/xsom/impl/ParticleImpl.java
+++ xsom/src/com/sun/xml/xsom/impl/ParticleImpl.java
@@ -1,11 +1,21 @@
 /*
- * @(#)$Id: ParticleImpl.java,v 1.1 2005-04-14 22:06:26 kohsuke Exp $
- *
- * Copyright 2001 Sun Microsystems, Inc. All Rights Reserved.
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the "License").  You may not use this file except
+ * in compliance with the License.
  * 
- * This software is the proprietary information of Sun Microsystems, Inc.  
- * Use is subject to license terms.
+ * You can obtain a copy of the license at
+ * https://jwsdp.dev.java.net/CDDLv1.0.html
+ * See the License for the specific language governing
+ * permissions and limitations under the License.
  * 
+ * When distributing Covered Code, include this CDDL
+ * HEADER in each file and include the License file at
+ * https://jwsdp.dev.java.net/CDDLv1.0.html  If applicable,
+ * add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your
+ * own identifying information: Portions Copyright [yyyy]
+ * [name of copyright owner]
  */
 package com.sun.xml.xsom.impl;
 
