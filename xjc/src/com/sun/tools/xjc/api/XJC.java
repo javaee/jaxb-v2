ffd74364c4f2ee4212ce6fb83f3165e246a1d0db
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2005-10-13 22:17:03 +0000

    removed methods that were deprecated and never implemented.

--- xjc/src/com/sun/tools/xjc/api/XJC.java
+++ xjc/src/com/sun/tools/xjc/api/XJC.java
@@ -19,12 +19,9 @@
  */
 package com.sun.tools.xjc.api;
 
-import com.sun.codemodel.JJavaName;
 import com.sun.tools.xjc.api.impl.j2s.JavaCompilerImpl;
 import com.sun.tools.xjc.api.impl.s2j.SchemaCompilerImpl;
 import com.sun.tools.xjc.reader.Util;
-import com.sun.xml.bind.api.impl.NameConverter;
-import com.sun.xml.bind.api.JAXBRIContext;
 
 /**
  * Entry point to the JAXB RI interface.
@@ -39,7 +36,7 @@ public final class XJC {
      * @return
      *      always return non-null object.
      */
-    public static final JavaCompiler createJavaCompiler() {
+    public static JavaCompiler createJavaCompiler() {
         return new JavaCompilerImpl();
     }
 
@@ -49,7 +46,7 @@ public final class XJC {
      * @return
      *      always return non-null object.
      */
-    public static final SchemaCompiler createSchemaCompiler() {
+    public static SchemaCompiler createSchemaCompiler() {
         return new SchemaCompilerImpl();
     }
 
@@ -65,83 +62,8 @@ public final class XJC {
      *      (there are certain namespace URIs with which this algorithm does not
      *      work --- such as ":::" as the URI.)
      */
-    public static final String getDefaultPackageName( String namespaceUri ) {
+    public static String getDefaultPackageName( String namespaceUri ) {
         if(namespaceUri==null)   throw new IllegalArgumentException();
         return Util.getPackageNameFromNamespaceURI( namespaceUri );
-    }
-
-    /**
-     * Obtains the expected MIME type for the given Java BLOB class.
-     *
-     * <p>
-     * The details of this table is going to be defined in the JAXB spec,
-     * but for example, this method encapsulates a table like
-     * "java.awt.Image" -> "image/*".
-     *
-     * @param className
-     *      a string that looks like a fully qualified Java class name.
-     * @return
-     *      null if the given class name is not recognized as a Java BLOB class.
-     */
-    public static final String getExpectedMimeTypeForBlobClass( String className ) {
-        // TODO: implement this method later
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * Obtains the Java BLOB class for the given expected MIME type.
-     *
-     *
-     * @param mimeType
-     *      a string that looks like a MIME type, such as "image/jpeg"
-     *      or "text/*".
-     * @return
-     *      always non-null valid string that looks like a fully qualifieid Java class name.
-     *      Such as "javax.activation.DataHandler".
-     * @throws IllegalArgumentException
-     *      if the given string is not a MIME type. 
-     */
-    public static final String getBlobClassForMimeType( String mimeType ) {
-        // TODO: implement this method later
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * Computes a Java identifier from a local name.
-     *
-     * <p>
-     * This method faithfully implements the name mangling rule as specified in the JAXB spec.
-     *
-     * <p>
-     * In JAXB, a collision with a Java reserved word (such as "return") never happens.
-     * Accordingly, this method may return an identifier that collides with reserved words.
-     *
-     * <p>
-     * Use {@link JJavaName#isJavaIdentifier(String)} to check for such collision.
-     *
-     * @return
-     *      Typically, this method returns "nameLikeThis".
-     *
-     * @see JJavaName#isJavaIdentifier(String)
-     *
-     * @deprecated use {@link JAXBRIContext#mangleNameToVariableName(String)}
-     */
-    public static final String mangleNameToVariableName(String localName) {
-        return NameConverter.standard.toVariableName(localName);
-    }
-
-    /**
-     * Computes a Java class name from a local name.
-     *
-     * <p>
-     * This method faithfully implements the name mangling rule as specified in the JAXB spec.
-     *
-     * @return
-     *      Typically, this method returns "NameLikeThis".
-     *
-     * @deprecated use {@link JAXBRIContext#mangleNameToClassName(String)} 
-     */
-    public static final String mangleNameToClassName(String localName) {
-        return NameConverter.standard.toClassName(localName);
     }
 }
