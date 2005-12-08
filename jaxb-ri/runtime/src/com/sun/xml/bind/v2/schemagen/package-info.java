a5abef9e77bba764dea97a891034d5ed524ebcee
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2005-12-08 22:08:40 +0000

    javadoc enhancement.

---
+++ jaxb-ri/runtime/src/com/sun/xml/bind/v2/schemagen/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Schema generator.
+ *
+ * This package hosts the code that generates schemas from
+ * {@link TypeInfoSet} (without the user interface.)
+ */
+package com.sun.xml.bind.v2.schemagen;
+
+import com.sun.xml.bind.v2.model.core.TypeInfoSet;
