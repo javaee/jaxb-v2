951e7f8c41eec632658cc7e161d2edf98af26d6d
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2006-01-27 00:08:24 +0000

    exposed 'mods'.

--- jaxb-ri/codemodel/src/com/sun/codemodel/JMethod.java
+++ jaxb-ri/codemodel/src/com/sun/codemodel/JMethod.java
@@ -415,12 +415,19 @@ public class JMethod extends JGenerifiableImpl implements JDeclaration, JAnnotat
 		}
 	}
 
-	/**
-	 * @return
-	 *      the current modifiers of this method.
-	 *      Always return non-null valid object. 
-	 */
-	public JMods getMods() {
+    /**
+     * @return
+     *      the current modifiers of this method.
+     *      Always return non-null valid object.
+     */
+    public JMods mods() {
+        return mods;
+    }
+
+    /**
+     * @deprecated use {@link #mods()} 
+     */
+    public JMods getMods() {
 		return mods;
 	}
 
