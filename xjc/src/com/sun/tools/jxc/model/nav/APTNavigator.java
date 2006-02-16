f7b478fa48de40ed236117bd55d8d9e05f7d3af9
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2006-02-16 22:10:02 +0000

    modified to use istack-commons.

--- xjc/src/com/sun/tools/jxc/model/nav/APTNavigator.java
+++ xjc/src/com/sun/tools/jxc/model/nav/APTNavigator.java
@@ -29,12 +29,14 @@ import com.sun.mirror.type.ReferenceType;
 import com.sun.mirror.type.TypeMirror;
 import com.sun.mirror.type.TypeVariable;
 import com.sun.mirror.type.WildcardType;
+import com.sun.mirror.type.VoidType;
 import com.sun.mirror.util.Declarations;
 import com.sun.mirror.util.SourcePosition;
 import com.sun.mirror.util.TypeVisitor;
 import com.sun.mirror.util.Types;
 import com.sun.xml.bind.v2.model.nav.Navigator;
 import com.sun.xml.bind.v2.runtime.Location;
+import com.sun.istack.tools.APTTypeVisitor;
 
 /**
  * {@link Navigator} implementation for APT.
@@ -336,36 +338,9 @@ public class APTNavigator implements Navigator<TypeMirror,TypeDeclaration,FieldD
     };
 
     /**
-     * Visitor that works on APT {@link TypeMirror} and computes a value.
-     */
-    abstract class APTTypeVisitor<ParamT> {
-        public final TypeMirror apply(TypeMirror type, ParamT param) {
-            if( type instanceof ArrayType)
-                return onArrayType((ArrayType)type,param);
-            if (type instanceof ClassType )
-                return onClassType((ClassType)type,param);
-            if (type instanceof InterfaceType )
-                return onInterfaceType((InterfaceType)type,param);
-            if (type instanceof TypeVariable )
-                return onTypeVariable((TypeVariable)type,param);
-            if(type instanceof WildcardType)
-                return onWildcard((WildcardType) type,param);
-            assert false;
-            throw new IllegalArgumentException();
-        }
-
-        protected abstract TypeMirror onArrayType(ArrayType type, ParamT param);
-        protected abstract TypeMirror onClassType(ClassType type, ParamT param);
-        protected abstract TypeMirror onInterfaceType(InterfaceType type, ParamT param);
-        protected abstract TypeMirror onTypeVariable(TypeVariable type, ParamT param);
-        protected abstract TypeMirror onWildcard(WildcardType type, ParamT param);
-
-    }
-
-    /**
      * Implements {@link #getBaseClass}.
      */
-    private final APTTypeVisitor<TypeDeclaration> baseClassFinder = new APTTypeVisitor<TypeDeclaration>(){
+    private final APTTypeVisitor<TypeMirror,TypeDeclaration> baseClassFinder = new APTTypeVisitor<TypeMirror,TypeDeclaration>(){
         public TypeMirror onClassType(ClassType type, TypeDeclaration sup) {
             TypeMirror r = onDeclaredType(type,sup);
             if(r!=null)     return r;
@@ -377,6 +352,14 @@ public class APTNavigator implements Navigator<TypeMirror,TypeDeclaration,FieldD
             }
 
             return null;
+        }
+
+        protected TypeMirror onPrimitiveType(PrimitiveType type, TypeDeclaration param) {
+            return type;
+        }
+
+        protected TypeMirror onVoidType(VoidType type, TypeDeclaration param) {
+            return type;
         }
 
         public TypeMirror onInterfaceType(InterfaceType type, TypeDeclaration sup) {
