8c0a9578a24a0a3426db6c8b3b63a98cf999a0e2
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2006-03-01 19:39:30 +0000

    hiding the @generateElementProperty customization as requested by Sekahr.

--- xjc/src/com/sun/tools/xjc/reader/xmlschema/bindinfo/BIGlobalBinding.java
+++ xjc/src/com/sun/tools/xjc/reader/xmlschema/bindinfo/BIGlobalBinding.java
@@ -271,7 +271,7 @@ public final class BIGlobalBinding extends AbstractDeclarationImpl {
     @XmlAttribute
     boolean generateElementClass = false;
 
-    @XmlAttribute
+    // @XmlAttribute
     Boolean generateElementProperty = null;
 
     @XmlAttribute
