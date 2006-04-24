409a1ec127774baefe69fef97181075ed9d42a4e
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2006-04-24 22:36:01 +0000

    base class properties need to be the first.

--- xjc/src/com/sun/tools/xjc/api/impl/s2j/AbstractMappingImpl.java
+++ xjc/src/com/sun/tools/xjc/api/impl/s2j/AbstractMappingImpl.java
@@ -64,55 +64,61 @@ abstract class AbstractMappingImpl<InfoT extends CElement> implements Mapping {
      * Derived classes can use this method to implement {@link #calcDrilldown}.
      */
     protected List<Property> buildDrilldown(CClassInfo typeBean) {
-        List<Property> result = new ArrayList<Property>();
+        List<Property> result;
 
-        for( ; typeBean!=null; typeBean=typeBean.getBaseClass() ) {
-            for( CPropertyInfo p : typeBean.getProperties() ) {
-                if (p instanceof CElementPropertyInfo) {
-                    CElementPropertyInfo ep = (CElementPropertyInfo) p;
-    // wrong. A+,B,C is eligible for drill-down.
-    //                if(ep.isCollection())
-    //                    // content model like A+,B,C is not eligible
-    //                    return null;
+        CClassInfo bc = typeBean.getBaseClass();
+        if(bc!=null) {
+            result = buildDrilldown(bc);
+            if(result==null)
+                return null;        // aborted
+        } else
+            result = new ArrayList<Property>();
 
-                    List<? extends CTypeRef> ref = ep.getTypes();
-                    if(ref.size()!=1)
-                        // content model like (A|B),C is not eligible
-                        return null;
+        for( CPropertyInfo p : typeBean.getProperties() ) {
+            if (p instanceof CElementPropertyInfo) {
+                CElementPropertyInfo ep = (CElementPropertyInfo) p;
+// wrong. A+,B,C is eligible for drill-down.
+//                if(ep.isCollection())
+//                    // content model like A+,B,C is not eligible
+//                    return null;
 
-                    result.add(createPropertyImpl(ep,ref.get(0).getTagName()));
-                } else
-                if (p instanceof ReferencePropertyInfo) {
-                    CReferencePropertyInfo rp = (CReferencePropertyInfo) p;
-
-                    Collection<CElement> elements = rp.getElements();
-                    if(elements.size()!=1)
-                        return null;
-
-                    CElement ref = elements.iterator().next();
-                    if(ref instanceof ClassInfo) {
-                        result.add(createPropertyImpl(rp,ref.getElementName()));
-                    } else {
-                        CElementInfo eref = (CElementInfo)ref;
-                        if(!eref.getSubstitutionMembers().isEmpty())
-                            return null;    // elements with a substitution group isn't qualified for the wrapper style
-
-                        // JAX-WS doesn't want to see JAXBElement, so we have to hide it for them.
-                        ElementAdapter fr;
-                        if(rp.isCollection())
-                            fr = new ElementCollectionAdapter(parent.outline.getField(rp), eref);
-                        else
-                            fr = new ElementSingleAdapter(parent.outline.getField(rp), eref);
-
-                        result.add(new PropertyImpl(this,
-                            fr, eref.getElementName()));
-                    }
-                } else
-                    // to be eligible for the wrapper style, only elements are allowed.
-                    // according to the JAX-RPC spec 2.3.1.2, element refs are disallowed
+                List<? extends CTypeRef> ref = ep.getTypes();
+                if(ref.size()!=1)
+                    // content model like (A|B),C is not eligible
                     return null;
 
-            }
+                result.add(createPropertyImpl(ep,ref.get(0).getTagName()));
+            } else
+            if (p instanceof ReferencePropertyInfo) {
+                CReferencePropertyInfo rp = (CReferencePropertyInfo) p;
+
+                Collection<CElement> elements = rp.getElements();
+                if(elements.size()!=1)
+                    return null;
+
+                CElement ref = elements.iterator().next();
+                if(ref instanceof ClassInfo) {
+                    result.add(createPropertyImpl(rp,ref.getElementName()));
+                } else {
+                    CElementInfo eref = (CElementInfo)ref;
+                    if(!eref.getSubstitutionMembers().isEmpty())
+                        return null;    // elements with a substitution group isn't qualified for the wrapper style
+
+                    // JAX-WS doesn't want to see JAXBElement, so we have to hide it for them.
+                    ElementAdapter fr;
+                    if(rp.isCollection())
+                        fr = new ElementCollectionAdapter(parent.outline.getField(rp), eref);
+                    else
+                        fr = new ElementSingleAdapter(parent.outline.getField(rp), eref);
+
+                    result.add(new PropertyImpl(this,
+                        fr, eref.getElementName()));
+                }
+            } else
+                // to be eligible for the wrapper style, only elements are allowed.
+                // according to the JAX-RPC spec 2.3.1.2, element refs are disallowed
+                return null;
+
         }
 
         return result;
