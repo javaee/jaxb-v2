192a79b2a265bd2d87e53d9c31ca9214f7b232be
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2006-04-26 17:22:38 +0000

    removed XJC API methods that are no longer in use.

--- jaxb-ri/xjc/src/com/sun/tools/xjc/api/impl/s2j/PropertyImpl.java
+++ jaxb-ri/xjc/src/com/sun/tools/xjc/api/impl/s2j/PropertyImpl.java
@@ -1,18 +1,11 @@
 package com.sun.tools.xjc.api.impl.s2j;
 
-import java.io.StringWriter;
-
 import javax.xml.namespace.QName;
 
-import com.sun.codemodel.JBlock;
 import com.sun.codemodel.JCodeModel;
-import com.sun.codemodel.JExpr;
-import com.sun.codemodel.JFormatter;
 import com.sun.codemodel.JType;
-import com.sun.codemodel.JVar;
 import com.sun.tools.xjc.api.Mapping;
 import com.sun.tools.xjc.api.Property;
-import com.sun.tools.xjc.outline.FieldAccessor;
 import com.sun.tools.xjc.outline.FieldOutline;
 
 /**
@@ -39,32 +32,7 @@ public /*for BSH*/ final class PropertyImpl implements Property {
         return elementName;
     }
 
-    protected final FieldAccessor createAccessor(String bean) {
-        return fr.create(JExpr.direct(bean));
-    }
-
-    protected final String writeBlock(JBlock block) {
-        StringWriter sw = new StringWriter();
-        JFormatter f = new JFormatter(sw);
-        block.state(f);
-        return sw.toString();
-    }
-
     public final JType type() {
         return fr.getRawType();
-    }
-
-    public String setValue(String bean, String var, String uniqueName) {
-        JBlock block = new JBlock();
-        createAccessor(bean).fromRawValue(block,uniqueName,JExpr.direct(var));
-        return writeBlock(block);
-    }
-
-    public String getValue(String bean, String var, String uniqueName) {
-        JBlock block = new JBlock();
-        JVar $var = block.decl(fr.getRawType(),var);
-        JBlock inner = block.block();
-        createAccessor(bean).toRawValue(inner,$var);
-        return writeBlock(inner);
     }
 }
