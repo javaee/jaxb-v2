08e570f5e9c99c675532abc8a048bcac1818478a
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2006-07-24 21:37:40 +0000

    [NEWS:jaxb] Fixed 199.

--- jaxb-ri/xjc/src/com/sun/tools/xjc/reader/xmlschema/bindinfo/OptionalPropertyMode.java
+++ jaxb-ri/xjc/src/com/sun/tools/xjc/reader/xmlschema/bindinfo/OptionalPropertyMode.java
@@ -1,12 +1,17 @@
 package com.sun.tools.xjc.reader.xmlschema.bindinfo;
 
+import javax.xml.bind.annotation.XmlEnumValue;
+
 /**
  * Represents three constants of globalBindings/@optionalProperty.
  *
  * @author Kohsuke Kawaguchi
  */
 public enum OptionalPropertyMode {
+    @XmlEnumValue("primitive")
     PRIMITIVE,
+    @XmlEnumValue("wrapper")
     WRAPPER,
+    @XmlEnumValue("isSet")
     ISSET
 }
