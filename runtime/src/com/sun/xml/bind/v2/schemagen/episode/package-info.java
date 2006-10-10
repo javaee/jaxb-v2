ad06830d7d795e3f2ed5683ac4a149489bdc2d65
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2006-10-10 17:24:40 +0000

    fixed a bug in episode generation.

--- runtime/src/com/sun/xml/bind/v2/schemagen/episode/package-info.java
+++ runtime/src/com/sun/xml/bind/v2/schemagen/episode/package-info.java
@@ -2,4 +2,8 @@
  * TXW interfaces for writing episode file, which is what XJC needs to
  * handle separate compilation.
  */
+@XmlNamespace(WellKnownNamespace.JAXB)
 package com.sun.xml.bind.v2.schemagen.episode;
+
+import com.sun.xml.txw2.annotation.XmlNamespace;
+import com.sun.xml.bind.v2.WellKnownNamespace;
