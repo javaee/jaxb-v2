527bee0066ef993d96464803dfb79532622ec411
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2006-10-27 19:16:52 +0000

    null cast should always succeed.

--- jaxb-ri/runtime/src/com/sun/xml/bind/v2/util/TypeCast.java
+++ jaxb-ri/runtime/src/com/sun/xml/bind/v2/util/TypeCast.java
@@ -10,6 +10,8 @@ public class TypeCast {
      * Makes sure that a map contains the right type, and returns it to the desirable type.
      */
     public static <K,V> Map<K,V> checkedCast( Map<?,?> m, Class<K> keyType, Class<V> valueType ) {
+        if(m==null)
+            return null;
         for (Map.Entry e : m.entrySet()) {
             if(!keyType.isInstance(e.getKey()))
                 throw new ClassCastException(e.getKey().getClass().toString());
