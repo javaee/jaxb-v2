ba03e75801c1bcc4098355890f29a91db3adb757
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2006-11-14 02:28:53 +0000

    fixed a build error

--- jaxb-ri/reflection/src/org/jvnet/jaxb/reflection/JAXBModelFactory.java
+++ jaxb-ri/reflection/src/org/jvnet/jaxb/reflection/JAXBModelFactory.java
@@ -72,7 +72,7 @@ public abstract class JAXBModelFactory {
         ErrorHandler errorHandler,
         Class... classes ) {
 
-        RuntimeModelBuilder builder = new RuntimeModelBuilder(reader,Collections.emptyMap(),null);
+        RuntimeModelBuilder builder = new RuntimeModelBuilder(null,reader,null);
         builder.setErrorHandler(errorHandler);
         for( Class c : classes )
             builder.getTypeInfo(new Ref<Type,Class>(c));
