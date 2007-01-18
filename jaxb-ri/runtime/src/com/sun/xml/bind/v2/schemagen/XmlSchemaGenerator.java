0f8354dbe3d31adf4483a8b7f8637d54870aeda1
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2007-01-18 18:56:17 +0000

    fixed #300.

--- jaxb-ri/runtime/src/com/sun/xml/bind/v2/schemagen/XmlSchemaGenerator.java
+++ jaxb-ri/runtime/src/com/sun/xml/bind/v2/schemagen/XmlSchemaGenerator.java
@@ -1153,8 +1153,9 @@ public final class XmlSchemaGenerator<T,C,F,M> {
             }
 
 
-            final Tree choice = Tree.makeGroup(GroupKind.CHOICE, children).makeRepeated(rp.isCollection());
-            // it's a curious omission that XmlElementRef doesn't have required()
+            final Tree choice = Tree.makeGroup(GroupKind.CHOICE, children).makeRepeated(rp.isCollection()).makeOptional(rp.isCollection());
+            // it's a curious omission that XmlElementRef doesn't have required().
+            // instead right now a collection will make it [0,unbounded]
 
 
             final QName ename = rp.getXmlName();
