eba537b391adb660aeaf8323832d59fa108d8bc5
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2007-05-31 22:46:16 +0000

    looks like these was something wrong with these 3 files.

--- runtime/src/com/sun/xml/bind/AccessorFactoryImpl.java
+++ runtime/src/com/sun/xml/bind/AccessorFactoryImpl.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- * 
+ *
  * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- * 
+ *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
  * and Distribution License("CDDL") (collectively, the "License").  You
@@ -10,7 +10,7 @@
  * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
  * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
- * 
+ *
  * When distributing the software, include this License Header Notice in each
  * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
  * Sun designates this particular file as subject to the "Classpath" exception
@@ -19,9 +19,9 @@
  * Header, with the fields enclosed by brackets [] replaced by your own
  * identifying information: "Portions Copyrighted [year]
  * [name of copyright owner]"
- * 
+ *
  * Contributor(s):
- * 
+ *
  * If you wish your version of this file to be governed by only the CDDL or
  * only the GPL Version 2, indicate your decision by adding "[Contributor]
  * elects to include this software in this distribution under the [CDDL or GPL
@@ -38,8 +38,10 @@ package com.sun.xml.bind;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+
+import javax.xml.bind.JAXBException;
+
 import com.sun.xml.bind.v2.runtime.reflect.Accessor;
-import com.sun.xml.bind.api.AccessorException;
 
 public class AccessorFactoryImpl implements AccessorFactory {
 
@@ -55,7 +57,7 @@ public class AccessorFactoryImpl implements AccessorFactory {
      * Access a field of the class. 
      *
      * @param bean the class to be processed.
-     * @param f the field within the class to be accessed.
+     * @param field the field within the class to be accessed.
      * @param readOnly  the isStatic value of the field's modifier.
      * @return Accessor the accessor for this field
      *
