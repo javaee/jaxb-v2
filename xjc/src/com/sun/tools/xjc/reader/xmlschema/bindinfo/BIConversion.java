1ff85872123d4b22cc48b9f3dc5e60a9349561ff
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2007-06-20 17:57:46 +0000

    fixed 6556352,

--- xjc/src/com/sun/tools/xjc/reader/xmlschema/bindinfo/BIConversion.java
+++ xjc/src/com/sun/tools/xjc/reader/xmlschema/bindinfo/BIConversion.java
@@ -265,9 +265,11 @@ public abstract class BIConversion extends AbstractDeclarationImpl {
         private String printMethodFor(XSSimpleType owner) {
             if(printMethod!=null)   return printMethod;
 
-            String method = getConversionMethod("print",owner);
-            if(method!=null)
-                return method;
+            if(inMemoryType.unboxify().isPrimitive()) {
+                String method = getConversionMethod("print",owner);
+                if(method!=null)
+                    return method;
+            }
 
             return "toString";
         }
@@ -275,10 +277,12 @@ public abstract class BIConversion extends AbstractDeclarationImpl {
         private String parseMethodFor(XSSimpleType owner) {
             if(parseMethod!=null)   return parseMethod;
 
-            String method = getConversionMethod("parse", owner);
-            if(method!=null) {
-                // this cast is necessary for conversion between primitive Java types
-                return '('+inMemoryType.unboxify().fullName()+')'+method;
+            if(inMemoryType.unboxify().isPrimitive()) {
+                String method = getConversionMethod("parse", owner);
+                if(method!=null) {
+                    // this cast is necessary for conversion between primitive Java types
+                    return '('+inMemoryType.unboxify().fullName()+')'+method;
+                }
             }
 
             return "new";
