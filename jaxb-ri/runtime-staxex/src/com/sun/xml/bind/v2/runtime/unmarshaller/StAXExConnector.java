275e105481957c398049126b854cc06aa68b8518
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2007-08-02 17:51:14 +0000

    performance improvement. Handle DataHandler-backed binary data more efficiently.

--- jaxb-ri/runtime-staxex/src/com/sun/xml/bind/v2/runtime/unmarshaller/StAXExConnector.java
+++ jaxb-ri/runtime-staxex/src/com/sun/xml/bind/v2/runtime/unmarshaller/StAXExConnector.java
@@ -67,7 +67,10 @@ final class StAXExConnector extends StAXStreamConnector {
             if(pcdata instanceof org.jvnet.staxex.Base64Data) {
                 org.jvnet.staxex.Base64Data bd = (org.jvnet.staxex.Base64Data) pcdata;
                 Base64Data binary = new Base64Data();
-                binary.set( bd.get(), bd.getDataLen(), bd.getMimeType() );
+                if(binary.hasData())
+                    binary.set(bd.getDataHandler());
+                else
+                    binary.set( bd.get(), bd.getDataLen(), bd.getMimeType() );
                 // we make an assumption here that the binary data shows up on its own
                 // not adjacent to other text. So it's OK to fire it off right now.
                 visitor.text(binary);
