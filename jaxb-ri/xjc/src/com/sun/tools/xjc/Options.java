f15cf994b6ddd06fce7477c44803e52998fd5607
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2008-05-20 22:33:14 +0000

    prevent people from shooting themselves in the foot by requiring the -extension switch for 2.2.

--- jaxb-ri/xjc/src/com/sun/tools/xjc/Options.java
+++ jaxb-ri/xjc/src/com/sun/tools/xjc/Options.java
@@ -783,6 +783,11 @@ public class Options
         if( schemaLanguage==null )
             schemaLanguage = guessSchemaLanguage();
 
+        if(target==SpecVersion.V2_2 && isExtensionMode())
+            throw new BadCommandLineException(
+                "Currently 2.2 is still not finalized yet, so using it requires the -extension switch." +
+                "NOTE THAT 2.2 SPEC MAY CHANGE BEFORE IT BECOMES FINAL.");
+
         if(pluginLoadFailure!=null)
             throw new BadCommandLineException(
                 Messages.format(Messages.PLUGIN_LOAD_FAILURE,pluginLoadFailure));
