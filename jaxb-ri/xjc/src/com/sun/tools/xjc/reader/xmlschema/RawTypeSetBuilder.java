9f19e8459ab58feff5aeea46a4b69db02052398b
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2008-06-23 17:11:57 +0000

    bug fix for bug 6629052

--- jaxb-ri/xjc/src/com/sun/tools/xjc/reader/xmlschema/RawTypeSetBuilder.java
+++ jaxb-ri/xjc/src/com/sun/tools/xjc/reader/xmlschema/RawTypeSetBuilder.java
@@ -287,7 +287,12 @@ public class RawTypeSetBuilder implements XSTermVisitor {
             if(parent.refs.size() > 1 && p.getAdapter() != null)
                 return RawTypeSet.Mode.MUST_BE_REFERENCE;
 
-            return RawTypeSet.Mode.SHOULD_BE_TYPEREF;
+            if(target.hasClass())
+                // if the CElementInfo was explicitly bound to a class (which happen if and only if
+                // the user requested so, then map that to reference property so that the user sees a class
+                return RawTypeSet.Mode.CAN_BE_TYPEREF;
+            else
+                return RawTypeSet.Mode.SHOULD_BE_TYPEREF;
         }
 
         protected boolean isListOfValues() {
