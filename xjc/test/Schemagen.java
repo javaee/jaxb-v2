35e95aff06827a06ab461e6fdbed55a24ec1533e
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2008-06-26 16:39:16 +0000

    adding another set of drivers.

---
+++ xjc/test/Schemagen.java
@@ -0,0 +1,10 @@
+/**
+ * Another entry point with consistent names with the script.
+ * 
+ * @author Kohsuke Kawaguchi
+ */
+public class Schemagen {
+    public static void main(String[] args) throws Exception {
+        SchemaGenerator.main(args);
+    }
+}
