5efa41cdd262cf9c9b511ba667be430600c99691
Martin Grebac <martin.grebac@oracle.com>
2008-11-10 17:13:06 +0000

    Merge from jaxb-2_1-branch.

--- jaxb-ri/xjc/src/com/sun/tools/xjc/api/SpecVersion.java
+++ jaxb-ri/xjc/src/com/sun/tools/xjc/api/SpecVersion.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- * 
+ *
  * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- * 
+ *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
  * and Distribution License("CDDL") (collectively, the "License").  You
@@ -10,7 +10,7 @@
  * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
  * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
- * 
+ *
  * When distributing the software, include this License Header Notice in each
  * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
  * Sun designates this particular file as subject to the "Classpath" exception
@@ -19,9 +19,9 @@
  * Header, with the fields enclosed by brackets [] replaced by your own
  * identifying information: "Portions Copyrighted [year]
  * [name of copyright owner]"
- * 
+ *
  * Contributor(s):
- * 
+ *
  * If you wish your version of this file to be governed by only the CDDL or
  * only the GPL Version 2, indicate your decision by adding "[Contributor]
  * elects to include this software in this distribution under the [CDDL or GPL
@@ -37,12 +37,12 @@
 package com.sun.tools.xjc.api;
 
 /**
- * Represents the spec version constant. 
+ * Represents the spec version constant.
  *
  * @author Kohsuke Kawaguchi
  */
 public enum SpecVersion {
-    V2_0, V2_1;
+    V2_0, V2_1, V2_2;
 
     /**
      * Returns true if this version is equal or later than the given one.
@@ -52,18 +52,19 @@ public enum SpecVersion {
     }
 
     /**
-     * Parses "2.0" and "2.1" into the {@link SpecVersion} object.
+     * Parses "2.0", "2.1", and "2.2" into the {@link SpecVersion} object.
      *
      * @return null for parsing failure.
      */
     public static SpecVersion parse(String token) {
         if(token.equals("2.0"))
             return V2_0;
-        else
         if(token.equals("2.1"))
             return V2_1;
+        if(token.equals("2.2"))
+            return V2_2;
         return null;
     }
 
-    public static final SpecVersion LATEST = V2_1;
+    public static final SpecVersion LATEST = V2_2;
 }
