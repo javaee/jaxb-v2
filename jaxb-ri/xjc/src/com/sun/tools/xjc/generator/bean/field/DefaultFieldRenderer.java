1beda2306ce70ab86bc370389f3025c13beaddb3
Martin Grebac <martin.grebac@oracle.com>
2008-11-11 13:59:50 +0000

    Merge from jaxb-2_1-branch.

--- jaxb-ri/xjc/src/com/sun/tools/xjc/generator/bean/field/DefaultFieldRenderer.java
+++ jaxb-ri/xjc/src/com/sun/tools/xjc/generator/bean/field/DefaultFieldRenderer.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- * 
+ *
  * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- * 
+ *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
  * and Distribution License("CDDL") (collectively, the "License").  You
@@ -10,7 +10,7 @@
  * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
  * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
- * 
+ *
  * When distributing the software, include this License Header Notice in each
  * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
  * Sun designates this particular file as subject to the "Classpath" exception
@@ -19,9 +19,9 @@
  * Header, with the fields enclosed by brackets [] replaced by your own
  * identifying information: "Portions Copyrighted [year]
  * [name of copyright owner]"
- * 
+ *
  * Contributor(s):
- * 
+ *
  * If you wish your version of this file to be governed by only the CDDL or
  * only the GPL Version 2, indicate your decision by adding "[Contributor]
  * elects to include this software in this distribution under the [CDDL or GPL
@@ -38,8 +38,12 @@ package com.sun.tools.xjc.generator.bean.field;
 import java.util.ArrayList;
 
 import com.sun.tools.xjc.generator.bean.ClassOutlineImpl;
+import com.sun.tools.xjc.model.CElement;
 import com.sun.tools.xjc.model.CPropertyInfo;
+import com.sun.tools.xjc.model.CReferencePropertyInfo;
 import com.sun.tools.xjc.outline.FieldOutline;
+import java.io.Serializable;
+import java.util.Set;
 
 /**
  * Default implementation of the FieldRendererFactory
@@ -55,7 +59,7 @@ import com.sun.tools.xjc.outline.FieldOutline;
 final class DefaultFieldRenderer implements FieldRenderer {
 
     private final FieldRendererFactory frf;
-    
+
     /**
      * Use {@link FieldRendererFactory#getDefault()}.
      */
@@ -67,16 +71,16 @@ final class DefaultFieldRenderer implements FieldRenderer {
         this.frf = frf;
         this.defaultCollectionFieldRenderer = defaultCollectionFieldRenderer;
     }
-    
+
     private FieldRenderer defaultCollectionFieldRenderer;
 
 
     public FieldOutline generate(ClassOutlineImpl outline, CPropertyInfo prop) {
         return decideRenderer(outline,prop).generate(outline,prop);
     }
-    
+
     private FieldRenderer decideRenderer(ClassOutlineImpl outline, CPropertyInfo prop) {
-        
+
         if (prop instanceof CReferencePropertyInfo) {
             CReferencePropertyInfo p = (CReferencePropertyInfo)prop;
             if (p.isDummy()) {
@@ -86,7 +90,7 @@ final class DefaultFieldRenderer implements FieldRenderer {
                 return frf.getContentList(outline.parent().getCodeModel().ref(ArrayList.class).narrow(Serializable.class));
             }
         }
-        
+
         if(!prop.isCollection()) {
             // non-collection field
 
@@ -99,11 +103,11 @@ final class DefaultFieldRenderer implements FieldRenderer {
                 // otherwise use the default non-collection field
                 return frf.getSingle();
         }
-        
+
         if( defaultCollectionFieldRenderer==null ) {
             return frf.getList(outline.parent().getCodeModel().ref(ArrayList.class));
         }
-        
+
         // this field is a collection field.
         // use untyped list as the default. This is consistent
         // to the JAXB spec.
--- jaxb-ri/xjc/src/com/sun/tools/xjc/generator/bean/field/UntypedListFieldRenderer.java
+++ jaxb-ri/xjc/src/com/sun/tools/xjc/generator/bean/field/UntypedListFieldRenderer.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- * 
+ *
  * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- * 
+ *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
  * and Distribution License("CDDL") (collectively, the "License").  You
@@ -10,7 +10,7 @@
  * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
  * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
- * 
+ *
  * When distributing the software, include this License Header Notice in each
  * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
  * Sun designates this particular file as subject to the "Classpath" exception
@@ -19,9 +19,9 @@
  * Header, with the fields enclosed by brackets [] replaced by your own
  * identifying information: "Portions Copyrighted [year]
  * [name of copyright owner]"
- * 
+ *
  * Contributor(s):
- * 
+ *
  * If you wish your version of this file to be governed by only the CDDL or
  * only the GPL Version 2, indicate your decision by adding "[Contributor]
  * elects to include this software in this distribution under the [CDDL or GPL
@@ -41,17 +41,25 @@ import com.sun.tools.xjc.model.CPropertyInfo;
 import com.sun.tools.xjc.outline.FieldOutline;
 
 /**
- * 
- * 
+ *
+ *
  * @author
  *     Kohsuke Kawaguchi (kohsuke.kawaguchi@sun.com)
  */
 public final class UntypedListFieldRenderer implements FieldRenderer {
 
     private JClass coreList;
+    private boolean dummy;
+    private boolean content;
 
-    protected UntypedListFieldRenderer( JClass coreList ) {
+    protected UntypedListFieldRenderer( JClass coreList) {
+        this(coreList, false, false);
+    }
+
+    protected UntypedListFieldRenderer( JClass coreList, boolean dummy, boolean content) {
         this.coreList = coreList;
+        this.dummy = dummy;
+        this.content = content;
     }
 
     public FieldOutline generate(ClassOutlineImpl context, CPropertyInfo prop) {
