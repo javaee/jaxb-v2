dbb820084196da505d19807fdfe254c45b3cc785
Martin Grebac <martin.grebac@oracle.com>
2008-12-15 16:33:45 +0000

    Unused import.

--- txw2/src/runtime/com/sun/xml/txw2/annotation/XmlValue.java
+++ txw2/src/runtime/com/sun/xml/txw2/annotation/XmlValue.java
@@ -21,11 +21,7 @@
 package com.sun.xml.txw2.annotation;
 
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
 import static java.lang.annotation.ElementType.METHOD;
-
-import com.sun.xml.txw2.DatatypeWriter;
-
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
