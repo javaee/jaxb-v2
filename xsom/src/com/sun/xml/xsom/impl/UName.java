d7f547d9381b0ddeeb34678bd936831deec64587
Pavel Bucek <pavel.bucek@oracle.com>
2008-12-15 14:30:47 +0000

    Issue 540; XSComplexType.getAttributeUse(String,String) always return null UName was used in HashMap without overriden equals and hashCode methods.

--- xsom/src/com/sun/xml/xsom/impl/UName.java
+++ xsom/src/com/sun/xml/xsom/impl/UName.java
@@ -74,7 +74,33 @@ public final class UName {
     public String getName() { return localName; }
     public String getNamespaceURI() { return nsUri; }
     public String getQualifiedName() { return qname; }
-    
+
+
+    // Issue 540; XSComplexType.getAttributeUse(String,String) always return null
+    // UName was used in HashMap without overriden equals and hashCode methods.
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof UName) {
+            UName u = (UName)obj;
+
+            return ((this.getName().compareTo(u.getName()) == 0) &&
+                    (this.getNamespaceURI().compareTo(u.getNamespaceURI()) == 0) &&
+                    (this.getQualifiedName().compareTo(u.getQualifiedName()) == 0));
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 13 * hash + (this.nsUri != null ? this.nsUri.hashCode() : 0);
+        hash = 13 * hash + (this.localName != null ? this.localName.hashCode() : 0);
+        hash = 13 * hash + (this.qname != null ? this.qname.hashCode() : 0);
+        return hash;
+    }
+
     /**
      * Compares {@link UName}s by their names.
      */
