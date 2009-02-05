937f9936592162607a7df597bbe6b5825979fd4b
Martin Grebac <martin.grebac@oracle.com>
2009-02-05 14:35:00 +0000

    Merge from trunk - add GFv3 module descriptor.

---
+++ glassfish/v3/jaxb_proto.py
@@ -0,0 +1,10 @@
+pkg = {
+    "name"          : "jaxb",
+    "version"       : "2.1.10,0-0.1",
+    "attributes"    : { "pkg.summary" : "JAXB API and RI",
+                        "pkg.description" : 
+"JAXB Specification Reference Implementation (under dual CDDL+GPL license)",
+                     "info.classification" : "Frameworks"  },
+    "dirtrees" : [ "glassfish"],
+    "licenses" : { "CDDL+GPLv2.txt" : { "license" : "CDDL+GPL" }}
+}
