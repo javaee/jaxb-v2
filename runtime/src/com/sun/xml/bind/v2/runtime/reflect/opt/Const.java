f4ea471f6b2ff2defcd418fa44fbde9ed4db46d9
Martin Grebac <martin.grebac@oracle.com>
2009-09-14 13:07:49 +0000

    Fix of 490 - Const is incorrectly hidden from optimized classes.

--- runtime/src/com/sun/xml/bind/v2/runtime/reflect/opt/Const.java
+++ runtime/src/com/sun/xml/bind/v2/runtime/reflect/opt/Const.java
@@ -39,13 +39,13 @@ package com.sun.xml.bind.v2.runtime.reflect.opt;
 /**
  * @author Kohsuke Kawaguchi
  */
-final class Const {
-    static byte default_value_byte = 0;
-    static boolean default_value_boolean = false;
-    static char default_value_char = 0;
-    static float default_value_float = 0;
-    static double default_value_double = 0;
-    static int default_value_int = 0;
-    static long default_value_long = 0;
-    static short default_value_short = 0;
+public final class Const {
+    public static byte default_value_byte = 0;
+    public static boolean default_value_boolean = false;
+    public static char default_value_char = 0;
+    public static float default_value_float = 0;
+    public static double default_value_double = 0;
+    public static int default_value_int = 0;
+    public static long default_value_long = 0;
+    public static short default_value_short = 0;
 }
