f7aaf03cb599edf6458223baafaa5bacfee43d78
Martin Grebac <martin.grebac@oracle.com>
2009-09-30 17:21:00 +0000

    Forgotten commit for fixing synchronization.

--- runtime/src/com/sun/xml/bind/v2/runtime/reflect/opt/AccessorInjector.java
+++ runtime/src/com/sun/xml/bind/v2/runtime/reflect/opt/AccessorInjector.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- * 
+ *
  * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- * 
+ *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
  * and Distribution License("CDDL") (collectively, the "License").  You
@@ -10,7 +10,7 @@
  * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
  * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
- * 
+ *
  * When distributing the software, include this License Header Notice in each
  * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
  * Sun designates this particular file as subject to the "Classpath" exception
@@ -19,9 +19,9 @@
  * Header, with the fields enclosed by brackets [] replaced by your own
  * identifying information: "Portions Copyrighted [year]
  * [name of copyright owner]"
- * 
+ *
  * Contributor(s):
- * 
+ *
  * If you wish your version of this file to be governed by only the CDDL or
  * only the GPL Version 2, indicate your decision by adding "[Contributor]
  * elects to include this software in this distribution under the [CDDL or GPL
@@ -74,17 +74,20 @@ class AccessorInjector {
             ClassLoader cl = beanClass.getClassLoader();
             if(cl==null)    return null;    // how do I inject classes to this "null" class loader? for now, back off.
 
-            Class c = Injector.find(cl,newClassName);
-            if(c==null) {
-                byte[] image = tailor(templateClassName,newClassName,replacements);
-//                try {
-//                    new FileOutputStream("debug.class").write(image);
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-                if(image==null)
-                    return null;
-                c = Injector.inject(cl,newClassName,image);
+            Class c = null;
+            synchronized (AccessorInjector.class) {
+                c = Injector.find(cl,newClassName);
+                if(c==null) {
+                    byte[] image = tailor(templateClassName,newClassName,replacements);
+    //                try {
+    //                    new FileOutputStream("debug.class").write(image);
+    //                } catch (IOException e) {
+    //                    e.printStackTrace();
+    //                }
+                    if(image==null)
+                        return null;
+                    c = Injector.inject(cl,newClassName,image);
+                }
             }
             return c;
         } catch(SecurityException e) {
