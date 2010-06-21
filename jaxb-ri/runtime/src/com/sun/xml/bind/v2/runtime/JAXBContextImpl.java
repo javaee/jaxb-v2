6b694c9e72fcf6b9c9848a5ba63df47b6ec5fd20
Martin Grebac <martin.grebac@oracle.com>
2010-06-21 14:16:39 +0000

    Add ability to supress security warnings when using reflection - merge from trunk.

--- jaxb-ri/runtime/src/com/sun/xml/bind/v2/runtime/JAXBContextImpl.java
+++ jaxb-ri/runtime/src/com/sun/xml/bind/v2/runtime/JAXBContextImpl.java
@@ -234,6 +234,11 @@ public final class JAXBContextImpl extends JAXBRIContext {
      */
     public final boolean retainPropertyInfo;
 
+    /**
+     * Supress reflection accessor warnings.
+     */
+    public final boolean supressAccessorWarnings;
+
     private WeakReference<RuntimeTypeInfoSet> typeInfoSetCache;
 
     private @NotNull RuntimeAnnotationReader annotaitonReader;
@@ -279,6 +284,7 @@ public final class JAXBContextImpl extends JAXBRIContext {
         this.classes = builder.classes;
         this.xmlAccessorFactorySupport = builder.xmlAccessorFactorySupport;
         this.allNillable = builder.allNillable;
+        this.supressAccessorWarnings = builder.supressAccessorWarnings;
 
         Collection<TypeReference> typeRefs = builder.typeRefs;
 
@@ -1050,6 +1056,7 @@ public final class JAXBContextImpl extends JAXBRIContext {
     public static class JAXBContextBuilder {
 
         private boolean retainPropertyInfo = false;
+        private boolean supressAccessorWarnings = false;
         private String defaultNsUri = "";
         private @NotNull RuntimeAnnotationReader annotationReader = new RuntimeInlineAnnotationReader();
         private @NotNull Map<Class,Class> subclassReplacements = Collections.emptyMap();
@@ -1062,6 +1069,7 @@ public final class JAXBContextImpl extends JAXBRIContext {
         public JAXBContextBuilder() {};
 
         public JAXBContextBuilder(JAXBContextImpl baseImpl) {
+            this.supressAccessorWarnings = baseImpl.supressAccessorWarnings;
             this.retainPropertyInfo = baseImpl.retainPropertyInfo;
             this.defaultNsUri = baseImpl.defaultNsUri;
             this.annotationReader = baseImpl.annotaitonReader;
@@ -1075,6 +1083,11 @@ public final class JAXBContextImpl extends JAXBRIContext {
 
         public JAXBContextBuilder setRetainPropertyInfo(boolean val) {
             this.retainPropertyInfo = val;
+            return this;
+        }
+
+        public JAXBContextBuilder setSupressAccessorWarnings(boolean val) {
+            this.supressAccessorWarnings = val;
             return this;
         }
 
