31a1f986451f23fede604012fd3b132a2be8d52b
Pavel Bucek <pavel.bucek@oracle.com>
2010-08-24 16:45:31 +0000

    license update

--- jaxb-ri/xjc/src/com/sun/tools/xjc/generator/bean/BeanGenerator.java
+++ jaxb-ri/xjc/src/com/sun/tools/xjc/generator/bean/BeanGenerator.java
@@ -33,6 +33,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
+
 package com.sun.tools.xjc.generator.bean;
 
 import static com.sun.tools.xjc.outline.Aspect.EXPOSED;
