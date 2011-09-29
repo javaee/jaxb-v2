f40c258dcc903487a6885fc538f8504b7eb09dbd
Lukas Jungmann <lukas.jungmann@oracle.com>
2011-09-29 15:07:58 +0000

    JAX_WS-921: wsimport task locks jars and doesn't release it after completion

--- xjc/src/com/sun/tools/xjc/XJC2Task.java
+++ xjc/src/com/sun/tools/xjc/XJC2Task.java
@@ -434,6 +434,7 @@ public class XJC2Task extends Task {
 
     private void doXJC() throws BuildException {
         ClassLoader old = Thread.currentThread().getContextClassLoader();
+        AntClassLoader acl = null;
         try {
             if (classpath != null) {
                 for (String pathElement : classpath.list()) {
@@ -445,11 +446,15 @@ public class XJC2Task extends Task {
                 }
             }
             // set the user-specified class loader so that XJC will use it.
-            Thread.currentThread().setContextClassLoader(new AntClassLoader(getProject(),classpath));
+            Thread.currentThread().setContextClassLoader(acl = new AntClassLoader(getProject(),classpath));
             _doXJC();
         } finally {
             // restore the context class loader
             Thread.currentThread().setContextClassLoader(old);
+            // close AntClassLoader
+            if (acl != null) {
+                acl.cleanup();
+            }
         }
     }
 
--- xjc/src/com/sun/tools/xjc/api/util/APTClassLoader.java
+++ xjc/src/com/sun/tools/xjc/api/util/APTClassLoader.java
@@ -124,6 +124,12 @@ public final class APTClassLoader extends URLClassLoader {
             return defineClass(name,buf,0,buf.length);
         } catch (IOException e) {
             throw new ClassNotFoundException(name,e);
+        } finally {
+            try {
+                is.close();
+            } catch (IOException ioe) {
+                //ignore
+            }
         }
     }
 
