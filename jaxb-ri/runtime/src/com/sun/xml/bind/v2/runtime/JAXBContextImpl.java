5e27afa7fb67c7b202c983dce3f96a5fc2f8deec
Martin Grebac <martin.grebac@oracle.com>
2013-03-21 17:34:14 +0100

    Method moved to test classes - no reason to keep it inside of jaxb context. Blocks moxy testing.

--- jaxb-ri/runtime/src/com/sun/xml/bind/v2/runtime/JAXBContextImpl.java
+++ jaxb-ri/runtime/src/com/sun/xml/bind/v2/runtime/JAXBContextImpl.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -907,19 +907,6 @@ public final class JAXBContextImpl extends JAXBRIContext {
             // impossible given that JAXBRIContext has been successfully built in the first place
             throw new AssertionError(e);
         }
-    }
-
-    /**
-     * Used for testing.
-     */
-    public SchemaOutputResolver createTestResolver() {
-        return new SchemaOutputResolver() {
-            public Result createOutput(String namespaceUri, String suggestedFileName) {
-                SAXResult r = new SAXResult(new DefaultHandler());
-                r.setSystemId(suggestedFileName);
-                return r;
-            }
-        };
     }
 
     @Override
