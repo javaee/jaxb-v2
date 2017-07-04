f1ef0676987b83b134b8c97fe99083423f7f3311
Lukas Jungmann <lukas.jungmann@oracle.com>
2017-07-04 11:11:05 +0200

    update copyrights
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- jaxb-ri/samples/src/main/samples/namespace-prefix/src/NamespacePrefixMapperImpl.java
+++ jaxb-ri/samples/src/main/samples/namespace-prefix/src/NamespacePrefixMapperImpl.java
@@ -1,29 +1,31 @@
-import com.sun.xml.bind.marshaller.NamespacePrefixMapper;
-
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- * 
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- * 
+ *
+ * Copyright (c) 1997-2017 Oracle and/or its affiliates. All rights reserved.
+ *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
  * and Distribution License("CDDL") (collectively, the "License").  You
- * may not use this file except in compliance with the License. You can obtain
- * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
- * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://oss.oracle.com/licenses/CDDL+GPL-1.1
+ * or LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
- * 
+ *
  * When distributing the software, include this License Header Notice in each
- * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
- * Sun designates this particular file as subject to the "Classpath" exception
- * as provided by Sun in the GPL Version 2 section of the License file that
- * accompanied this code.  If applicable, add the following below the License
- * Header, with the fields enclosed by brackets [] replaced by your own
- * identifying information: "Portions Copyrighted [year]
- * [name of copyright owner]"
- * 
+ * file and include the License file at LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
  * Contributor(s):
- * 
  * If you wish your version of this file to be governed by only the CDDL or
  * only the GPL Version 2, indicate your decision by adding "[Contributor]
  * elects to include this software in this distribution under the [CDDL or GPL
@@ -36,13 +38,15 @@ import com.sun.xml.bind.marshaller.NamespacePrefixMapper;
  * holder.
  */
 
+import com.sun.xml.bind.marshaller.NamespacePrefixMapper;
+
 class NamespacePrefixMapperImpl extends NamespacePrefixMapper {
 
     /**
      * Returns a preferred prefix for the given namespace URI.
-     * 
+     *
      * This method is intended to be overrided by a derived class.
-     * 
+     *
      * @param namespaceUri
      *      The namespace URI for which the prefix needs to be found.
      *      Never be null. "" is used to denote the default namespace.
@@ -52,24 +56,24 @@ class NamespacePrefixMapperImpl extends NamespacePrefixMapper {
      *      parameter. Typicall this value comes from the QName.getPrefix
      *      to show the preference of the content tree. This parameter
      *      may be null, and this parameter may represent an already
-     *      occupied prefix. 
+     *      occupied prefix.
      * @param requirePrefix
      *      If this method is expected to return non-empty prefix.
      *      When this flag is true, it means that the given namespace URI
      *      cannot be set as the default namespace.
-     * 
+     *
      * @return
      *      null if there's no prefered prefix for the namespace URI.
      *      In this case, the system will generate a prefix for you.
-     * 
+     *
      *      Otherwise the system will try to use the returned prefix,
      *      but generally there's no guarantee if the prefix will be
      *      actually used or not.
-     * 
+     *
      *      return "" to map this namespace URI to the default namespace.
      *      Again, there's no guarantee that this preference will be
      *      honored.
-     * 
+     *
      *      If this method returns "" when requirePrefix=true, the return
      *      value will be ignored and the system will generate one.
      */
@@ -77,7 +81,7 @@ class NamespacePrefixMapperImpl extends NamespacePrefixMapper {
         // I want this namespace to be mapped to "xsi"
         if( "http://www.w3.org/2001/XMLSchema-instance".equals(namespaceUri) )
             return "xsi";
-         
+
         // I want the namespace foo to be the default namespace.
         if( "http://www.example.com/foo".equals(namespaceUri) )
             return "";
@@ -85,13 +89,13 @@ class NamespacePrefixMapperImpl extends NamespacePrefixMapper {
         // and the namespace bar will use "b".
         if( "http://www.example.com/bar".equals(namespaceUri) )
             return "b";
-            
+
         // otherwise I don't care. Just use the default suggestion, whatever it may be.
         return suggestion;
     }
-    
-    
-    
+
+
+
     /**
      * Returns a list of namespace URIs that should be declared
      * at the root element.
@@ -112,7 +116,7 @@ class NamespacePrefixMapperImpl extends NamespacePrefixMapper {
      * <p>
      * If you know in advance that you are going to use a certain set of
      * namespace URIs, you can override this method and have the marshaller
-     * declare those namespace URIs at the root element. 
+     * declare those namespace URIs at the root element.
      * <p>
      * For example, by returning <code>new String[]{"urn:foo"}</code>,
      * the marshaller will produce:
@@ -127,16 +131,16 @@ class NamespacePrefixMapperImpl extends NamespacePrefixMapper {
      * <xmp></pre>
      * <p>
      * To control prefixes assigned to those namespace URIs, use the
-     * {@link #getPreferredPrefix} method. 
-     * 
+     * {@link #getPreferredPrefix} method.
+     *
      * @return
      *      A list of namespace URIs as an array of {@link String}s.
      *      This method can return a length-zero array but not null.
      *      None of the array component can be null. To represent
      *      the empty namespace, use the empty string <code>""</code>.
-     * 
+     *
      * @since
-     *      JAXB RI 1.0.2 
+     *      JAXB RI 1.0.2
      */
     public String[] getPreDeclaredNamespaceUris() {
         return new String[] { "urn:abc", "urn:def" };
