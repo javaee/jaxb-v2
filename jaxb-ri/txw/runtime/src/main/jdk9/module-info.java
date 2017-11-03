7d6e9fdb265c7968404b67a8d6576b41dd9af0cd
Jack Li <zheng.jun.li@oracle.com>
2017-11-03 01:42:21 -0700

    modularise txw

--- jaxb-ri/txw/runtime/src/main/jdk9/module-info.java
+++ jaxb-ri/txw/runtime/src/main/jdk9/module-info.java
@@ -38,51 +38,10 @@
  * holder.
 */
 
-/**
- * The XML Binding (JAXB) RI modularization implementation.
- *
- * <p> This module is upgradeable.
- *
- * @uses javax.xml.bind.JAXBContextFactory
- *
- * @since 9
- */
-module java.xml.bind.imp {
-    requires java.xml.bind;
-    requires java.compiler;
-    requires java.desktop;
-    requires java.logging;
-    requires jdk.unsupported;
-
-    requires transitive java.activation;
+module jaxb.txw2 {
     requires transitive java.xml;
 
-    requires jaxb.txw2;
-    requires FastInfoset;
-    requires stax.ex;
-    requires istack.commons.runtime;
-
-    exports com.sun.xml.bind;
-    exports com.sun.xml.bind.annotation;
-    exports com.sun.xml.bind.api;
-    exports com.sun.xml.bind.api.impl;
-    exports com.sun.xml.bind.marshaller;
-    exports com.sun.xml.bind.unmarshaller;
-    exports com.sun.xml.bind.util;
-    exports com.sun.xml.bind.v2;
-    exports com.sun.xml.bind.v2.model.annotation;
-    exports com.sun.xml.bind.v2.model.core;
-    exports com.sun.xml.bind.v2.model.impl;
-    exports com.sun.xml.bind.v2.model.nav;
-    opens com.sun.xml.bind.v2.model.nav;
-    exports com.sun.xml.bind.v2.model.runtime;
-    exports com.sun.xml.bind.v2.model.util;
-    exports com.sun.xml.bind.v2.runtime;
-    exports com.sun.xml.bind.v2.runtime.unmarshaller;
-    exports com.sun.xml.bind.v2.schemagen;
-    exports com.sun.xml.bind.v2.schemagen.episode;
-    exports com.sun.xml.bind.v2.schemagen.xmlschema;
-    exports com.sun.xml.bind.v2.util;
-
-    uses javax.xml.bind.JAXBContextFactory;
+    exports com.sun.xml.txw2;
+    exports com.sun.xml.txw2.annotation;
+    exports com.sun.xml.txw2.output;
 }
